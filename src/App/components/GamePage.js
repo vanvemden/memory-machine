d11e6fe0e8bc3d57d35a9837a7658b7e3266d6d8
Marco van Vemden <vanvemden@gmail.com>
2022-05-16 22:11:27 -0700

    Add class name to game page

--- src/App/components/GamePage.js
+++ src/App/components/GamePage.js
@@ -2,10 +2,10 @@ import ScoreBoard from './ScoreBoard';
 import CardGrid from './CardGrid';
 
 const GamePage = () => (
-  <>
+  <div className='GamePage'>
     <ScoreBoard />
     <CardGrid />;
-  </>
+  </div>
 );
 
 export default GamePage;
